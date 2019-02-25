5a2b48d5fd8416b228afb0801d51179003d5b74f
kevinykuo <kevinykuo@gmail.com>
2019-02-25 17:33:56 -0500

    update signature of spark_read_tfrecord

--- R/loader.R
+++ R/loader.R
@@ -3,13 +3,14 @@
 #' Read a TFRecord file as a Spark DataFrame.
 #'
 #' @param sc A spark conneciton.
-#' @param name The name to assign to the newly generated table.
+#' @param name The name to assign to the newly generated table or the path to the file. Note that if a path is provided for the `name` argument
+#'   then one cannot specify a name.
 #' @param path The path to the file. Needs to be accessible from the cluster. Supports the "hdfs://", "s3a://" and "file://" protocols.
 #' @param schema (Currently unsupported.) Schema of TensorFlow records.  If not provided, the schema is inferred from TensorFlow records.
 #' @param record_type Input format of TensorFlow records. By default it is Example.
 #' @param overwrite Boolean; overwrite the table with the given name if it already exists?
 #' @export
-spark_read_tfrecord <- function(sc, name, path, schema = NULL,
+spark_read_tfrecord <- function(sc, name = NULL, path = name, schema = NULL,
                                 record_type = c("Example", "SequenceExample"),
                                 overwrite = TRUE) {
   record_type <- match.arg(record_type)
